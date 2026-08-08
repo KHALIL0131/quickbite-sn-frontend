abc237065a5293b2c91c0c4e2c22304e09f12a8c
Ibrahima Faye <ibrahima.faye42@unchk.edu.sn>
2026-08-08 17:45:49 +0000

    Correction URL backend production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serverUrl: 'https://quickbite-api.onrender.com',
-  apiUrl: 'https://quickbite-api.onrender.com/api',
-  socketUrl: 'https://quickbite-api.onrender.com'
+  serverUrl: 'https://quickbite-sn-backend.onrender.com',
+  apiUrl: 'https://quickbite-sn-backend.onrender.com/api',
+  socketUrl: 'https://quickbite-sn-backend.onrender.com'
 };
